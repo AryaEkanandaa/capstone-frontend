5f7983c76d042c00fbd2610cfe352b3a03d0fb52
Arya Ekananda <147296239+AryaEkanandaa@users.noreply.github.com>
2025-12-15 06:47:31 +0800

    Update tailwind.config.cjs

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -2,10 +2,21 @@
 module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/*/.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        floatSlow: "float 7s ease-in-out infinite",
+        floatFast: "float 4.5s ease-in-out infinite",
+},
+      keyframes: {
+        float: {
+          "0%, 100%": { transform: "translateY(0px)" },
+          "50%": { transform: "translateY(-40px)" },
+        },
+      },
+    },
   },
   plugins: [],
 };
